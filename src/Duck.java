81246d09f8e59423b32e8a0dffc4584c5e03f9b1
Fey <97257705+lawfey@users.noreply.github.com>
2022-04-21 15:38:06 -0400

    Fey pattern design exercise

--- src/Duck.java
+++ src/Duck.java
@@ -1,2 +1,21 @@
-public class Duck {
+public abstract class Duck {
+
+        //interface type
+    //instance variables hold a reference to a specific behavior at runtime
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    public Duck (){
+
+    }
+    public abstract void display();
+
+    public void performFly(){
+        flyBehavior.fly();
+    }
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+    public void swim(){
+        System.out.println("All ducks float, even decoys!");
+    }
 }
--- src/MallardDuck.java
+++ src/MallardDuck.java
@@ -1,2 +1,10 @@
-public class MallardDuck {
+public class MallardDuck extends  Duck  {
+    public MallardDuck(){
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    public void display(){
+        System.out.println("I'm a real Mallard duck");
+    }
 }
